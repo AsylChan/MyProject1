f974e6fe79af9cf1401ae55eed973c1c3c61e268
AsylChan <aseka.qanatbek@bk.ru>
2021-11-10 17:58:48 +0600

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 5;
+        double b = 8.5;
+        double c = a*b;
+        System.out.println(c);
+    }
+}
